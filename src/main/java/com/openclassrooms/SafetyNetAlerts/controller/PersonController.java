8461067bdec7c0b0741d0dc2bb552eab21104b89
kneerace <kneerace@gmail.com>
2025-03-03 16:34:15 -0500

    PersonController added with POST mapping to add Person to existing Person list

---
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/PersonController.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.SafetyNetAlerts.controller;
+
+import com.openclassrooms.SafetyNetAlerts.model.Person;
+import com.openclassrooms.SafetyNetAlerts.service.PersonalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // This means that this class is a Controller and is used where we the return is mapped to the view
+@RequestMapping("/person")  // This means URL's start with /person (after Application path) i.e. basePath localhost:8080/person
+public class PersonController {
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+
+    private PersonalService personalService;
+
+    public PersonController(PersonalService personalService) {
+        this.personalService = personalService;
+    } // end constructor
+
+    /*
+    http://localhost:8080/person
+This endpoint will provide the following via Http Post/Put/Delete:
+Add a new person.
+Update an existing person (at this time, assume that firstName and lastName do not change, but other
+fields can be modified).
+Delete a person. (Use a combination of firstName and lastName as a unique identifier)
+     */
+    @PostMapping
+    public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+
+        Person addedPerson = personalService.addPerson(person);
+        // return added person with status created
+//        return ResponseEntity.status(201).body(addedPerson);
+         return new ResponseEntity<>(addedPerson, HttpStatus.CREATED);
+
+    } // end addPerson
+
+} // end class
