5b6386925cfd76afff331f24387ba13bb921d657
kneerace <kneerace@gmail.com>
2025-03-03 11:20:00 -0500

    DataController : GET /flood/stations added

--- src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
@@ -1,9 +1,8 @@
 package com.openclassrooms.SafetyNetAlerts.controller;
 
-import com.openclassrooms.SafetyNetAlerts.model.ChildAlertResponse;
-import com.openclassrooms.SafetyNetAlerts.model.FireResponse;
-import com.openclassrooms.SafetyNetAlerts.model.FireStationServiceResponse;
+import com.openclassrooms.SafetyNetAlerts.model.*;
 import com.openclassrooms.SafetyNetAlerts.service.FireStationService;
+import com.openclassrooms.SafetyNetAlerts.service.FloodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +14,12 @@ import java.util.List;
 public class DataController {
 
     private final FireStationService fireStationService;
+    private final FloodService floodService;
 
     @Autowired
-    public DataController(FireStationService fireStationService){
+    public DataController(FireStationService fireStationService, FloodService floodService) {
         this.fireStationService = fireStationService;
+        this.floodService =  floodService;
     }
 
     @GetMapping("/firestation")
@@ -45,5 +46,12 @@ public class DataController {
 
         return fireStationService.getFireStationByAddress(address);
     } // end of getFireStationByAddress
+    @GetMapping("/flood/stations")
+    public List<HouseholdByStation> getHouseholdByFireStations(@RequestParam List<String> fireStations) {
+
+        return floodService.getHouseholdByFireStations(fireStations);
+    } // end of getHouseholdByFireStations
+
+
     } // end of class
 
