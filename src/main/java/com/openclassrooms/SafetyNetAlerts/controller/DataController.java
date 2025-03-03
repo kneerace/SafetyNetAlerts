46b9a430e3e32ee48d85c53c2c0efad5e95963f7
kneerace <kneerace@gmail.com>
2025-03-03 12:16:22 -0500

    DataController : GET method for "/presonalInfo" and "/communityEmail" added

--- src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
@@ -3,11 +3,14 @@ package com.openclassrooms.SafetyNetAlerts.controller;
 import com.openclassrooms.SafetyNetAlerts.model.*;
 import com.openclassrooms.SafetyNetAlerts.service.FireStationService;
 import com.openclassrooms.SafetyNetAlerts.service.FloodService;
+import com.openclassrooms.SafetyNetAlerts.service.PersonalInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -15,11 +18,14 @@ public class DataController {
 
     private final FireStationService fireStationService;
     private final FloodService floodService;
+    private final PersonalInfoService personalInfoService ;
 
     @Autowired
-    public DataController(FireStationService fireStationService, FloodService floodService) {
+    public DataController(FireStationService fireStationService, FloodService floodService,
+                          PersonalInfoService personalInfoService) {
         this.fireStationService = fireStationService;
         this.floodService =  floodService;
+        this.personalInfoService = personalInfoService;
     }
 
     @GetMapping("/firestation")
@@ -46,12 +52,34 @@ public class DataController {
 
         return fireStationService.getFireStationByAddress(address);
     } // end of getFireStationByAddress
+
     @GetMapping("/flood/stations")
     public List<HouseholdByStation> getHouseholdByFireStations(@RequestParam List<String> fireStations) {
 
         return floodService.getHouseholdByFireStations(fireStations);
     } // end of getHouseholdByFireStations
 
+
+    @GetMapping("/personInfo")
+    public PersonalInfoResponse getPersonInfo(@RequestParam String firstName, @RequestParam String lastName) {
+//        logger.info("Request: /personInfo?firstname={}&lastname={}", firstName, lastName);
+        PersonalInfoResponse response = personalInfoService.getPersonInfo(firstName, lastName);
+//        logger.info("Response: {}", response);
+        return response;
+    }
+
+    @GetMapping("/communityEmail")
+    public ResponseEntity<EmailListResponse> getCommunityEmail(@RequestParam String city) {
+//        logger.info("Request: /communityEmail?city={}", city);
+//        List<String> emails = personalInfoService.getCommunityEmail(city);
+//        logger.info("Emails for city: {} are: {}", city, emails);
+        EmailListResponse emails = personalInfoService.getCommunityEmailList(city);
+//        return emails;
+        return emails.getEmailList().isEmpty()
+//                ? ResponseEntity.notFound().build()
+                ? ResponseEntity.ok(new EmailListResponse(Collections.emptyList()))  // returining empty json {}
+                : ResponseEntity.ok(emails);
+    } // end of getCommunityEmail
 
     } // end of class
 
