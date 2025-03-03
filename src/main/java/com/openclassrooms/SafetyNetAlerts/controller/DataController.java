3a7714af55838122266658c65546a9a6d6fe539f
kneerace <kneerace@gmail.com>
2025-03-03 05:20:39 -0500

    DataController: Optimized code removing logger statement

--- src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/DataController.java
@@ -2,8 +2,6 @@ package com.openclassrooms.SafetyNetAlerts.controller;
 
 import com.openclassrooms.SafetyNetAlerts.model.FireStationServiceResponse;
 import com.openclassrooms.SafetyNetAlerts.service.FireStationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +11,6 @@ public class DataController {
 
     private final FireStationService fireStationService;
 
-    private static final Logger logger = LoggerFactory.getLogger(DataController.class);
-
     @Autowired
     public DataController(FireStationService fireStationService){
         this.fireStationService = fireStationService;
@@ -23,18 +19,8 @@ public class DataController {
     @GetMapping("/firestation")
     public FireStationServiceResponse getFireStationByStationNumber( int stationNumber) {
 
-//        logger.info ("Request  received : /firestation?stationNumber={}", stationNumber);
+        return fireStationService.getFireStationByStationNumber(stationNumber);
 
-        FireStationServiceResponse fireStationServiceResponse = fireStationService.getFireStationByStationNumber(stationNumber);
-        if(fireStationServiceResponse !=null){
-//            logger.info("Response: {}", fireStationServiceResponse.getPersonServiced());
-
-        }
-        else {
-//            logger.warn("No data found for stationNumber: {}", stationNumber);
-        }
-
-        return fireStationServiceResponse;
     } // end of method
 } // end of class
 
