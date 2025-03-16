b3c93d64d0d98046c1f91706d774605bebdbff41
kneerace <kneerace@gmail.com>
2025-03-15 20:49:56 -0400

    EmailListResponse: added equals and hashCode overide

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/EmailListResponse.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/EmailListResponse.java
@@ -3,6 +3,8 @@ package com.openclassrooms.SafetyNetAlerts.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
+import java.util.Objects;
+
 public class EmailListResponse {
 
     private  List<String> emailList;
@@ -20,5 +22,18 @@ public class EmailListResponse {
     public void setEmailList(List<String> emailList) {
         this.emailList = emailList;
     } // end of setEmailList
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmailListResponse that = (EmailListResponse) o;
+        return Objects.equals(emailList, that.emailList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(emailList);
+    }
 
 } // end of EmailListResponse
